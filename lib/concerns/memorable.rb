35f20f13aee8db3adc37838039cf1114a5dca00e
Naz Khan <nazmuskhan@yahoo.com>
2017-03-22 00:40:55 -0400

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,21 @@
+module Memorable
+
+
+  def reset_all
+    @@songs.clear
+  end
+
+  def reset_all
+    self.all.clear
+  end
+
+  def count
+    @@artists.count
+  end
+
+  def count
+    self.all.count
+  end
+
+
+end
